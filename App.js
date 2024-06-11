6891916cb32306103730d5c9dc51f2f182f4c37c
jmeno1011 <jwhltn8282@gmail.com>
2024-06-11 15:35:48 +0900

    learn styles

--- App.js
+++ App.js
@@ -2,27 +2,31 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Anouther piece of text!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!" style={styles.textInput} />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello World</Text>
-      <Button title="Tab Me" />
+      <View>
+        <Text>List of goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "blue",
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8
   },
 });
