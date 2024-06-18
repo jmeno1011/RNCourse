bddde2ce7feed7081dd0f009c652e881930ce5dd
jmeno1011 <jwhltn8282@gmail.com>
2024-06-18 18:24:58 +0900

    learn ~50th lecture

--- App.js
+++ App.js
@@ -1,79 +1,9 @@
-import { useState } from "react";
-import { StyleSheet, View, FlatList, Button } from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { StyleSheet, View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [modalIsVisible, setModalIsVislbe] = useState(false);
-  const [courseGoals, setCourseGoal] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVislbe(true);
-  }
-
-  function addGaolHandler(enteredGoalText) {
-    setCourseGoal((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    endAddGoalHandler();
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVislbe(false);
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoal((currentCourseGoal) => {
-      return currentCourseGoal.filter((goal) => goal.id !== id);
-    });
-  }
-
-  return (
-    <>
-      <StatusBar />
-      <View style={styles.appContainer}>
-        <Button
-          title="Add New Goal"
-          color={"#B180f0"}
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          onAddGoal={addGaolHandler}
-          visible={modalIsVisible}
-          onCancel={endAddGoalHandler}
-        />
-        <View style={styles.goalsContainer} addGaolHandler={addGaolHandler}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  onDeleteItem={deleteGoalHandler}
-                  id={itemData.item.id}
-                />
-              );
-            }}
-            alwaysBounceVertical={false}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-          ></FlatList>
-        </View>
-      </View>
-    </>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    padding: 50,
-    paddingHorizontal: 16,
-  },
-  goalsContainer: {
-    flex: 5,
-  },
-});
+const styles = StyleSheet.create({});
